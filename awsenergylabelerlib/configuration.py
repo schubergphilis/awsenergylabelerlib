28f6d6ad53b30a47bd6bf0995f9f1fd19422c473
Costas Tyfoxylos <costas.tyf@gmail.com>
2023-06-05 16:10:56 +0300

    Fix for consolidated findings.

--- awsenergylabelerlib/configuration.py
+++ awsenergylabelerlib/configuration.py
@@ -105,7 +105,8 @@ DEFAULT_SECURITY_HUB_FILTER = {'UpdatedAt': [{'DateRange': {'Value': 7,
                                'RecordState': [{'Value': 'ARCHIVED',
                                                 'Comparison': 'NOT_EQUALS'}]}
 
-DEFAULT_SECURITY_HUB_FRAMEWORKS = {'aws-foundational-security-best-practices'}  # cis-aws-foundations-benchmark,
+# "aws-foundational-security-best-practices", "cis-aws-foundations-benchmark", "nist-800-53", "pci-dss"
+DEFAULT_SECURITY_HUB_FRAMEWORKS = {'aws-foundational-security-best-practices'}
 
 
 def get_available_security_hub_regions():
